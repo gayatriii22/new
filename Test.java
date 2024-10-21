2d738d4bcdc58c1e64db76f57edbbd16ee9d80e2
gayatriii22 <vu4f2223128@pvppcoe.ac.in>
2024-10-21 23:00:55 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
-public class README{
-
-  public static void main(String() args){
-    
+public class Test {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
 }
